eb58eda91dad84c48a4910c54320debf36854c42
vinodkmani <56358395+vinodkmani@users.noreply.github.com>
2021-05-14 13:45:10 +0100

    Add files via upload

---
+++ module.ts
@@ -0,0 +1,54 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface UserData {
+ vehicleId : string;
+tagId : string;
+tagStatus : string;
+dept : string;
+currentZone : string;
+batterylevel : string;
+lastMessageDate : string;
+lastGPSMessageDate : string;
+latestUserEdit : string;
+}
+
+/** Constants used to fill up our data base. */
+
+
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
+@Component({
+  selector: 'table-overview-example',
+  styleUrls: ['table-overview-example.css'],
+  templateUrl: 'table-overview-example.html',
+})
+export class TableOverviewExample implements AfterViewInit {
+  displayedColumns: string[] = ['vehicleId','tagId','tagStatus','dept','currentZone','batterylevel','lastMessageDate','lastGPSMessageDate','latestUserEdit'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
